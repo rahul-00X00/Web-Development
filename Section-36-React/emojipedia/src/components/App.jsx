ebae821b2c2ef8e9408f28df15ad4fec8b1004e0
rare <rahulreddin@gmail.com>
2024-01-31 22:43:27 +0530

    [IMP] emojipedia: improvement and styles

--- Section-36-React/emojipedia/src/components/App.jsx
+++ Section-36-React/emojipedia/src/components/App.jsx
@@ -1,22 +1,26 @@
 import Card from "./Card.jsx";
 import emojipedia from "../emoji_data.js";
+import "./styles.css";
 
 function createCard(emoji) {
-    return <Card 
-        key={emoji.id}
-        img={emoji.emoji}
-        text={emoji.name}
-        description={emoji.meaning}
+  return (
+    <Card
+      key={emoji.id}
+      name={emoji.name}
+      img={emoji.emoji}
+      text={emoji.name}
+      description={emoji.meaning}
     />
+  );
 }
 
 function App() {
-    return (
-        <div>
-            <h1>Emoji List Rendered using React</h1>
-            {emojipedia.map(createCard)}
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="heading">Emojipedia</h1>
+      <div className="card-container">{emojipedia.map(createCard)}</div>
+    </div>
+  );
 }
 
 export default App;
--- Section-36-React/emojipedia/src/components/Card.jsx
+++ Section-36-React/emojipedia/src/components/Card.jsx
@@ -1,9 +1,12 @@
+import "./styles.css";
+
 function Card(props) {
   return (
-    <div>
-      <span role="img" aria-label={props.text}>
+    <div class="card">
+      <span role="img" aria-label={props.text} className="emoji-size">
         {props.img}
       </span>
+      <h2>{props.name}</h2>
       <span> {props.description}</span>
     </div>
   );
